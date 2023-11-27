924e9129e3e314c68e95125d0c0160bfe0c3cab2
marcus5cents <152194539+marcus5cents@users.noreply.github.com>
2023-11-27 10:35:22 -0500

    Update LedBlink.ino

--- LedBlink.ino
+++ LedBlink.ino
@@ -5,10 +5,11 @@ void setup() {
   }
 }
 void loop() {
-  for(int i=2;i<2;i++){
-    digitalWrite(ledPins[i], HIGH);
-    delay(500);
-    digitalWrite(ledPins[i], LOW);
-  }
+  digitalWrite(ledPins[0], HIGH);
+  delay(500);
+  digitalWrite(ledPins[0], LOW;
+  digitalWrite(ledPins[1], HIGH);
+  delay(500);
+  digitalWrite(ledPins[1], LOW;
 }
 
